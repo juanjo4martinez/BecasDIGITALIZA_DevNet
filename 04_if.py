ef5662744343d5ab4236232273a6c0f609b1303c
Juanjo Martínez <47528725+juanjo4martinez@users.noreply.github.com>
2019-02-14 19:49:53 +0100

    Update 04_if.py

--- 04_if.py
+++ 04_if.py
@@ -1,5 +1,7 @@
+# Crear variable que almacena un número entero introducido por el usuario
 numero = int(input("Introduce un número: "))
 
+# Condicional 'if' - Según el número introducido, muestra si pertenece a un rango u otro
 if numero < 20:
     print("El número es menor que 20","->",numero)
 elif 20 < numero < 39:
