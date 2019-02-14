c47b274541f75600ae13dd94e8ed431b8b8f949b
Juanjo Martínez <47528725+juanjo4martinez@users.noreply.github.com>
2019-02-14 19:47:39 +0100

    Update 03_personal-info.py

--- 03_personal-info.py
+++ 03_personal-info.py
@@ -1,8 +1,9 @@
+# Crear variables que almacenan la entrada del usuario
 nombre = input("Nombre: ")
 apellidos = input("Apellidos: ")
 tlf = input("Teléfono: ")
 ciudad = input("Ciudad: ")
 edad = input("Edad: ")
 
+# Imprimir en pantalla la frase completa
 print("Tu nombre completo es",nombre,apellidos,", vives en",ciudad,"y tienes",edad,"años.","Tu número de teléfono es",tlf,".")
-
