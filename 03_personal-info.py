d9e1e920ab62f06171b9c8e70bc488989e00d132
Juanjo Martínez <47528725+juanjo4martinez@users.noreply.github.com>
2019-02-14 19:38:54 +0100

    Add files via upload

---
+++ 03_personal-info.py
@@ -0,0 +1,8 @@
+nombre = input("Nombre: ")
+apellidos = input("Apellidos: ")
+tlf = input("Teléfono: ")
+ciudad = input("Ciudad: ")
+edad = input("Edad: ")
+
+print("Tu nombre completo es",nombre,apellidos,", vives en",ciudad,"y tienes",edad,"años.","Tu número de teléfono es",tlf,".")
+
---
+++ 04_if.py
@@ -0,0 +1,10 @@
+numero = int(input("Introduce un número: "))
+
+if numero < 20:
+    print("El número es menor que 20","->",numero)
+elif 20 < numero < 39:
+    print("El número está entre 20 y 39","->",numero)
+elif 40 < numero < 59:
+    print("El número está entre 40 y 59","->",numero)
+else:
+    print("El número es mayor que 60","->",numero)
